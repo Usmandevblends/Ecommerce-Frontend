624a8e0933de0e68b1c00fb0c0ff47dccd176adf
usman <imdevusman@gmail.com>
2025-03-14 15:21:17 +0500

    `Added reselect dependency and memoized filtered products in FilterData component`

--- src/Pages/FilterData.jsx
+++ src/Pages/FilterData.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import ProductCard from '../Components/ProductCard'
+import React, { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import ProductCard from '../Components/ProductCard';
 
 const FilterData = () => {
-    const filterProducts = useSelector(state => state.product.filteredData)
+    const productState = useSelector(state => state.product);
+    const filterProducts = useMemo(() => productState?.filteredData || [], [productState]);
+
     return (
-        <div className=' mx-auto py-12 px-4 md:px-16 lg:px-24 '>
+        <div className='mx-auto py-12 px-4 md:px-16 lg:px-24'>
+            <h2 className='text-3xl font-bold text-center mb-8'>Filtered Products</h2>
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 cursor-pointer'>
                 {filterProducts.map((product, index) => (
                     <ProductCard product={product} key={index} />
                 ))}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default FilterData
+export default FilterData;
